9ae925aa84a828bc338984372a8e8d95e7e3b1ec
Son of Odin <2700975-son-of-odin@users.noreply.gitlab.com>
2021-01-11 15:01:18 -0500

    add debug lines

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -427,7 +427,7 @@ func (app *BaseApp) Query(req abci.RequestQuery) (res abci.ResponseQuery) {
 		return handleQueryCustom(app, path, req)
 	}
 
-	return sdkerrors.QueryResult(sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, fmt.Sprintf("unknown query path abci------: %+v", path)))
+	return sdkerrors.QueryResult(sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, fmt.Sprintf("unknown query path abci------: %+v", req.Path)))
 }
 
 // ListSnapshots implements the ABCI interface. It delegates to app.snapshotManager if set.
